a1f3368d241fcb72102ad73ed6890addee1f34b2
zsofetch <antolijaozsofia@gmail.com>
2025-09-08 11:57:54 +0800

    variation 3 function headers added

--- variation_3.c
+++ variation_3.c
@@ -7,4 +7,20 @@ typedef struct {
     int max;
 } List;
 
+List initialize(List L);
+List insertPos(List L, int data, int position);
+List deletePos(List L, int position);
+int locate(List L, int data);
+List insertSorted(List L, int data);
+void display(List L);
+List resize(List L);
+
+Dynamically allocate memory for the array using LENGTH
+Set max to defined LENGTH
+Set the count to 0
+Return List
+List initialize(List L) {
+
+}
+
 
